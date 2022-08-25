ceefbc2a39ecc6b8c428470b3559f6d01573f707
AnimalCarrot206 <dambrovskijvova@gmail.com>
2022-08-25 18:50:12 +0500

    подогнал под архитектуру

--- src/ServerScriptService/Server/Card.lua
+++ src/ServerScriptService/Server/Card.lua
@@ -1,22 +1,41 @@
 --!strict
 
 local Class = require(game.ReplicatedStorage.Shared.Class)
-local CustonEnum = require(game.ReplicatedStorage.Shared.CustomEnum)
+local CustomEnum = require(game.ReplicatedStorage.Shared.CustomEnum)
 local ModuleContainer = require(game.ReplicatedStorage.Shared.ModuleContainer)
 
 local Armory = require(game.ServerScriptService.Server.Armory)
 
-local cardsEnum = CustonEnum.new("Cards", {})
-local cardTypeEnum = CustonEnum.new("CardType", {})
+local cardsEnum = CustomEnum.new("Cards", {
+    ["Crack!"] = 0,
+    ["Miss"] = 1,
+    ["Ambush!"] = 2,
+    ["Lemonade"] = 3,
+    ["Drinks on me"] = 4,
+    ["Present"] = 5,
+    ["Cage"] = 6,
+    ["Blackmail"] = 7,
+    ["Thief"] = 8,
+    ["Reverse"] = 9,
+    ["Exchange"] = 10,
+    ["Duel"] = 11,
+    ["Move"] = 12,
+    ["Mayor's pardon"] = 13,
+})
+local cardTypeEnum = CustomEnum.new("CardType", {
+    OnePlayerUse = 0,
+    TwoPlayerUse = 1
+})
 
 
 local Card = Class:extend()
 
 Card.Container = game.ReplicatedStorage.Models.Cards
 
-function Card:new(cardName, type)
-    self.Name = cardName
-    self.Type = type
+function Card:new(cardName, type, overlap: any?)
+    self._name = cardName
+    self._type = type
+    self._overlap = overlap
 end
 
 function Card:destroy()
@@ -24,8 +43,16 @@ function Card:destroy()
     self = nil
 end
 
+function Card:getType()
+    return self._type
+end
+
 function Card:getName(): string
-    return self.Name :: string
+    return self._name :: string
+end
+
+function Card:getOvelrap(): any?
+    return self._overlap
 end
 
 function Card:use()
@@ -39,7 +66,7 @@ end
 local Crack = Card:extend()
 
 function Crack:new()
-    self.super:new("Crack")
+    self.super:new(CustomEnum.Cards["Crack!"].Name, CustomEnum.Cards["Crack!"].Value)
 end
 
 function Crack:use(player: Player, enemy: Player?)
