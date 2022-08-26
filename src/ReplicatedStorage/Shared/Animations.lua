3dd1e1f1d6e7f47164a428caaa3b7e6df7c0fb38
AnimalCarrot206 <dambrovskijvova@gmail.com>
2022-08-26 16:58:37 +0500

    Добавлен вспомогательный класс-хранилище анимаций

---
+++ src/ReplicatedStorage/Shared/Animations.lua
@@ -0,0 +1,26 @@
+--!strict
+local Class = require(game.ReplicatedStorage.Shared.Class)
+
+local Animations = Class:extend()
+
+Animations.PLAYER_SHOOT_ANIMATION_HIT = Instance.new("Animation")
+Animations.ENEMY_SHOOT_ANIMATION_HIT = Instance.new("Animation")
+
+Animations.PLAYER_SHOOT_ANIMATION_MISS = Instance.new("Animation")
+Animations.ENEMY_SHOOT_ANIMATION_MISS = Instance.new("Animation")
+
+Animations.PLAYER_TAKES_DAMAGE = Instance.new("Animation")
+
+Animations.PLAYER_USE_LEMONADE = Instance.new("Animation")
+
+function Animations:animatePlayer(player: Player, animation: Animation): AnimationTrack
+    local character = player.Character
+    local humanoid = character:FindFirstChildOfClass("Humanoid")
+    local animator = humanoid:FindFirstChildOfClass("Animator")
+
+    local animationTrack = animator:LoadAnimation(animation)
+
+    return animationTrack
+end
+
+return Animations
