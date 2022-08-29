0af5e3fd510bafd598530c621500e542b3300efe
AnimalCarrot206 <dambrovskijvova@gmail.com>
2022-08-29 11:36:41 +0500

    Подготовлен метод вычисления дальности

--- src/ServerScriptService/Server/Armory.lua
+++ src/ServerScriptService/Server/Armory.lua
@@ -2,7 +2,6 @@
 local Players = game:GetService("Players")
 
 local Class = require(game.ReplicatedStorage.Shared.Class)
-local ModuleContainer = require(game.ReplicatedStorage.Shared.ModuleContainer)
 local PlayerStats = require(game.ReplicatedStorage.Shared.PlayerStats)
 
 local RolesManager = require(game.ServerScriptService.Server.RolesManager)
@@ -16,11 +15,61 @@ local function _setPlayerAttributes(player: Player, gun)
     PlayerStats:setRange(player, gun:getRange())
 end
 
+local _calculateRange do
+
+    local sits: {{next: number,  previous: number, value: number}} = {
+        [1] = {next = 2, previous = 3, value = 1},
+        [2] = {next = 4, previous = 1, value = 2},
+        [3] = {next = 1, previous = 5, value = 3},
+        [4] = {next = 6, previous = 2, value = 4},
+        [5] = {next = 3, previous = 7, value = 5},
+        [6] = {next = 8, previous = 4, value = 6},
+        [7] = {next = 5, previous = 8, value = 7},
+        [8] = {next = 7, previous = 6, value = 8},
+    }
+    --[=[
+                1
+            2       3
+        4               5
+            6       7
+                8
+    ]=]
+    _calculateRange = function(playerASitPlace: number, playerBSitPlace: number)
+        if playerASitPlace + playerBSitPlace == 9  then
+            return 4
+        end
+
+		local current = sits[playerASitPlace]
+
+		local i = 0
+		
+		local isDeadendReached = false
+        while true do
+            if isDeadendReached == true then
+                current = sits[current.previous]
+            elseif isDeadendReached == false then
+                current = sits[current.next]
+            end
+            
+			i += 1
+            if playerASitPlace + current.value == 9 then
+				i = 0
+				isDeadendReached = true
+                current = sits[playerASitPlace]		
+			end
+			
+			if current.value == playerBSitPlace then
+				return i
+			end
+        end
+    end
+end
+
 function Armory:prepareGuns()
     local allPlayers = Players:GetPlayers()
 
     for _, player in ipairs(allPlayers) do
-        local newGun = Guns.rustyRevolver(player)
+        local newGun = Guns["Rusty revolver"]()
         table.insert(createdGuns, newGun)
 
         _setPlayerAttributes(player, newGun)
@@ -33,16 +82,15 @@ function Armory:disableGuns()
     end
 end
 
-function Armory:calculateRange(numA, numB, player: Player, enemy: Player)
-    local playerSitPlace = numA or player:GetAttribute("SitPlace") :: number
-    local enemySitPlace = numB or enemy:GetAttribute("SitPlace") :: number
+function Armory:calculateRange(playerA: Player, playerB: Player)
+    local playerASitPlace = PlayerStats:getPlayerSitPlace(playerA)
+    local playerBSitPlace = PlayerStats:getPlayerSitPlace(playerB)
+    
+    
+end
 
-    local range
-
-    if range < 0 then
-        range = -range
-    end
-    return range
+function Armory:test(num1, num2)
+    return _calculateRange(num1, num2)
 end
 
 function Armory:giveGun(player: Player, gunName: string)
