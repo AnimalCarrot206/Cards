e4531e18bafb3e776637c12e7a8bcb15954be390
AnimalCarrot206 <dambrovskijvova@gmail.com>
2022-08-29 11:38:07 +0500

    Пока на конструировании

--- src/ServerScriptService/Server/CardDecisionmaking.lua
+++ src/ServerScriptService/Server/CardDecisionmaking.lua
@@ -4,28 +4,4 @@ local Class = require(game.ReplicatedStorage.Shared.Class)
 
 local CardDecisionmaking = Class:extend()
 
-local CardDeckManager = nil :: any
-
-CardDecisionmaking.PlayerCard = nil
-CardDecisionmaking.EnemyCard = nil
-
-local function _getCard(player: Player, cardName: string)
-    local playerDeck = CardDeckManager:getPlayerDeck(player)
-    assert(playerDeck)
-
-    local foundCard = playerDeck:getCard(cardName)
-    assert(foundCard)
-    return foundCard
-end
-
-function CardDecisionmaking:setPlayerActivatedCard(player: Player, cardName: string)
-    self.PlayerCard = _getCard(player, cardName)
-end
-
-function CardDecisionmaking:setEnemyActivaredCard(enemy: Player, cardName: string)
-    self.EnemyCard = _getCard(enemy, cardName)
-end
-
-function CardDecisionmaking:makeDecision()
-    
-end
+local CardInputParser = require()
